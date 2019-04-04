fba1633da5220c73646cdf1ed27caeb4a14b7daa
kav0n9 <aaronznovak@gmail.com>
2019-04-04 17:23:18 +0200

    some more project

---
+++ Week01/day3/projects/third.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+    // - Create an array variable named `q`
+    //   with the following content: `[4, 5, 6, 7]`
+    // - Print the third element of `q`
+int q_array[] = {4, 5, 6, 7};
+std::cout << q_array[2] << std::endl;
+    return 0;
+}
