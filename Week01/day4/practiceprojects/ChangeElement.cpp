930b6cd2f85aceb2436969682896b6db9f6c6912
kav0n9 <aaronznovak@gmail.com>
2019-04-05 16:13:24 +0200

    element changed

---
+++ Week01/day4/practiceprojects/ChangeElement.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+int main(int argc, char* args[]) {
+
+    // - Create an array variable named `s`
+    //   with the following content: `[1, 2, 3, 8, 5, 6]`
+    // - Change the 8 to 4
+    // - Print the fourth element
+int s_array[] = {1, 2, 3, 4, 5, 6};
+std::cout << s_array[3] << std::endl;
+
+    return 0;
+}
