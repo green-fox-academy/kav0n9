95236347d74f2de0f6a82eb70b3e85cb0d3bc935
kav0n9 <aaronznovak@gmail.com>
2019-04-08 17:01:37 +0200

    reupload. making my repo nice

---
+++ Week01/day2/projects/ClassmatesWelcome/ClassmatesWelcome.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+
+    // Greet 3 of your classmates with this program, in three separate lines
+    // like:
+    //
+    // Hello, Esther!
+    // Hello, Mary!
+    // Hello, Joe!
+    std::cout << "Hello Zsofi!\nHello Bence!\nHello Szucsi!" << std::endl;
+    return 0;
+}
