ac62e9d59a730272f957073c0b56262c9e84e5e0
kav0n9 <aaronznovak@gmail.com>
2019-04-08 17:07:45 +0200

    greenfox coding hours

---
+++ Week01/day2/projects/CodingHours/CodingHours.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(int argc, char* args[]) {
+
+    // An average Green Fox attendee codes 6 hours daily
+    // The semester is 17 weeks long
+    //
+    // Print how many hours is spent with coding in a semester by an attendee,
+    // if the attendee only codes on workdays.
+    //
+    // Print the percentage of the coding hours in the semester if the average
+    // working hours weekly is 52
+    int a = 6;
+    int b = 5;
+    int c = 17;
+    int d = 52;
+
+    std::cout << a * b * c << std::endl;
+    std::cout << (510. / 884) * 100 << std::endl;
+    return 0;
+}
