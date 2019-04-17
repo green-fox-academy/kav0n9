dd11416dd36428c96583ef147c9d4d8ef5c2d726
kav0n9 <aaronznovak@gmail.com>
2019-04-17 16:01:39 +0200

    Blogpost not perfect yet

--- week3/day3/BlogPost/BlogPost.cpp
+++ week3/day3/BlogPost/BlogPost.cpp
@@ -3,3 +3,51 @@
 //
 
 #include "BlogPost.h"
+
+BlogPost::BlogPost(std::string _authorName, std::string _title, std::string _text, std::string _publlicationDate) {
+    setauthorName(_authorName);
+    settitle(_title);
+    settext(_text);
+    setpublicationDate(_publlicationDate);
+
+}
+
+BlogPost::BlogPost() {
+
+}
+
+void BlogPost::setauthorName(std::string authorName) {
+    _authorName = authorName;
+}
+
+void BlogPost::settitle(std::string title) {
+    _title = title;
+}
+
+void BlogPost::settext(std::string text) {
+    _text = text;
+}
+
+void BlogPost::setpublicationDate(std::string publicationDate) {
+    _publicationDate = publicationDate;
+}
+
+std::string BlogPost::getauthorName()
+{
+    return _authorName;
+}
+
+std::string BlogPost::gettitle()
+{
+    return _title;
+}
+
+std::string BlogPost::gettext()
+{
+    return _text;
+}
+
+std::string BlogPost::getpublicationDate()
+{
+    return _publicationDate;
+}
--- week3/day3/BlogPost/BlogPost.h
+++ week3/day3/BlogPost/BlogPost.h
@@ -6,11 +6,34 @@
 #define BLOGPOST_BLOGPOST_H
 
 
+#include <iostream>
 
 class BlogPost {
+public:
+    BlogPost(std::string authorName, std::string title, std::string text, std::string publlicationDate);
+
+    BlogPost();
+
+    void setauthorName(std::string authorName);
+
+    void settitle(std::string title);
+
+    void settext(std::string text);
+
+    void setpublicationDate(std::string publicationDate);
+
+    std::string getauthorName;
+    std::string gettitle;
+    std::string gettext;
+    std::string getpublicationDate;
+
+private:
+    std::string _authorName;
+    std::string _title;
+    std::string _text;
+    std::string _publicationDate;
 
 };
-
 
 
 #endif //BLOGPOST_BLOGPOST_H
