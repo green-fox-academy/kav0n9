2e2fb2d103466206f2d5a257fa68156d1916248b
kav0n9 <aaronznovak@gmail.com>
2019-04-18 14:16:10 +0200

    Animal.cpp uploaded

---
+++ week3/day3/Animal/Animal.cpp
@@ -0,0 +1,43 @@
+//
+// Created by aaron on 2019. 04. 17..
+//
+
+#include "Animal.h"
+
+void Animal::sethunger(int hunger)
+{
+    _hunger = hunger;
+}
+void Animal::setthirst(int thirst)
+{
+    _thirst = thirst;
+}
+
+int Animal::gethunger()
+{
+    return _hunger;
+}
+
+int Animal::getthirst()
+{
+    return _thirst;
+}
+
+void Animal::eat()
+{
+    int hunger = gethunger();
+    sethunger(hunger - 1);
+}
+
+void Animal::drink()
+{
+    int thirst = getthirst();
+    setthirst(thirst - 1);
+}
+void Animal::play()
+{
+    int hunger = gethunger();
+    sethunger(hunger + 1);
+    int thirst = getthirst();
+    setthirst(thirst + 1);
+}
