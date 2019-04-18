fbd727ed3499b10b45d1eaf87c2e4aa18ef2896c
kav0n9 <aaronznovak@gmail.com>
2019-04-18 14:16:24 +0200

    Animal.h uploaded

---
+++ week3/day3/Animal/Animal.h
@@ -0,0 +1,27 @@
+//
+// Created by aaron on 2019. 04. 17..
+//
+
+#ifndef ANIMAL_ANIMAL_H
+#define ANIMAL_ANIMAL_H
+
+#include <iostream>
+
+class Animal {
+public:
+    void sethunger(int hunger);
+    void setthirst(int thirst);
+    int gethunger ();
+    int getthirst();
+    void eat();
+    void drink();
+    void play();
+
+private:
+    int _hunger = 50;
+    int _thirst = 50;
+
+};
+
+
+#endif //ANIMAL_ANIMAL_H
